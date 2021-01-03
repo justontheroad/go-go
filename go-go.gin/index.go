d942df71a0b46e874250e9de6f0b7dcf223e0fa3
justontheroad <490421206@qq.com>
2021-01-03 23:45:27 +0800

    gin中间件

--- go-go.gin/index.go
+++ go-go.gin/index.go
@@ -204,6 +204,20 @@ func main() {
 		})
 	})
 
+	r.GET("/show-async", func(c *gin.Context) {
+		// 创建要在goroutine中使用的副本
+		cCp := c.Copy()
+		go func() {
+			// simulate a long task with time.Sleep(). 5 seconds
+			time.Sleep(1 * time.Second)
+			// 这里使用你创建的副本
+			log.Println("Done! in path " + cCp.Request.URL.Path)
+		}()
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
